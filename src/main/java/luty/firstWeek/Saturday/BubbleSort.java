664f542425c059c12db514db15c08492bc98e1ef
Szymon Szaniawski <szymonszaniawski31@gmail.com>
2020-02-01 15:53:35 +0100

    Added new BubbleSort that sorts tab of ints (II sposób)

--- src/main/java/luty/firstWeek/Saturday/BubbleSort.java
+++ src/main/java/luty/firstWeek/Saturday/BubbleSort.java
@@ -6,14 +6,30 @@ public class BubbleSort {
 
     public static void sort(int[] tab) {
 
+//        boolean swapped;
+//        do {
+//            swapped = false;
+//            for (int i = 1; i < tab.length; i++) {
+//                int leftObject = tab[i - 1];
+//                int rightObject = tab[i];
+//                if (leftObject > rightObject) {
+//                    int temp = leftObject;
+//                    tab[i - 1] = tab[i];
+//                    tab[i] = temp;
+//                    swapped = true;
+//                }
+//            }
+//        } while (swapped);
+
+//        II sposób
+
         boolean swapped;
         do {
             swapped = false;
             for (int i = 1; i < tab.length; i++) {
-                int leftObject = tab[i - 1];
-                int rightObject = tab[i];
-                if (leftObject > rightObject) {
-                    int temp = leftObject;
+
+                if (tab[i - 1] > tab[i]) {
+                    int temp = tab[i - 1];
                     tab[i - 1] = tab[i];
                     tab[i] = temp;
                     swapped = true;
