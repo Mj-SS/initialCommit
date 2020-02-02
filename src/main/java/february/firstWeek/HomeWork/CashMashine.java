380785252c6355038bef2eafc0f3a1bc0d33d395
Szymon Szaniawski <szymonszaniawski31@gmail.com>
2020-02-02 02:18:48 +0100

    Added new CashMashine it counts the number of banknotes

--- src/main/java/february/firstWeek/HomeWork/CashMashine.java
+++ src/main/java/february/firstWeek/HomeWork/CashMashine.java
@@ -1,13 +1,45 @@
 package february.firstWeek.HomeWork;
 
 public class CashMashine {
-    
-    public static void givenMoney (int a, int b, int c, int d, int e, int f) {
-        int n = 0;
 
+    public static int billCounter(int cashSum) {
+        int billCount = 0;
+        for (int i = 0; i < 6; i++){
+            int a = 0;
+            switch (i){
+                case 0:
+                    a = cashSum / 500;
+                    cashSum -= a * 500;
+                    billCount += a;
+                    break;
+                case 1:
+                    a = cashSum / 200;
+                    cashSum -= a * 200;
+                    billCount += a;
+                    break;
+                case 2:
+                    a = cashSum / 100;
+                    cashSum -= a * 100;
+                    billCount += a;
+                    break;
+                case 3:
+                    a = cashSum / 50;
+                    cashSum -= a * 50;
+                    billCount += a;
+                    break;
+                case 4:
+                    a = cashSum /20;
+                    cashSum -= a * 20;
+                    billCount += a;
+                    break;
+                case 5:
+                    a = cashSum/ 10;
+                    cashSum -= a * 10;
+                    billCount += a;
+                    break;
+            }
         }
-
-
+        return billCount;
     }
-
+}
 
